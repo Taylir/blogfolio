118fbefbf99fbe340bb05502f6b25b2090650065
Taylir <110134876+Taylir@users.noreply.github.com>
2023-01-20 12:05:49 -0500

    Landing Html and Css done, will be linking in future

---
+++ src/components/landingFolder/Landing.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import "./landingAssets/Landing.css";
+
+import {
+  ArrowDownwardRounded,
+  ArrowForwardRounded,
+  EmailRounded,
+  PlaylistAddCheckRounded,
+  ViewModuleRounded,
+} from "@mui/icons-material";
+
+const Landing = () => {
+  const scaleFactor = 1 / 20;
+
+  function moveClouds(event) {
+    const clouds = document.querySelector(".cloud");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    clouds.style.transform = `translate(${x}px, ${y}px)`;
+  }
+
+  return (
+    <div className="landing" onMouseMove={moveClouds}>
+      <div className="landing__header">
+        <h1>
+          Hey, I'm <span className="call-to">Taylor</span> 👋, <br /> a{" "}
+          <span className="call-to">Frontend Software Engineer</span>.
+        </h1>
+      </div>
+      <div className="about-me">
+        <h4>
+          I'm a 25 year-old American{" "}
+          <span className="call-to">Frontend Software Engineer</span> with a
+          strong passion for developing websites with great user interface and
+          user experience. I currently work on extremely difficult engineering
+          problems as well as working and learning with a team consisiting of
+          some of the most <span className="call-to"> talented</span> and
+          <span className="call-to"> experienced</span> software engineers every
+          day.
+        </h4>
+      </div>
+      <div className="contact-me">
+        <h5>
+          Feel free to contact me by clicking this. <pre> </pre>{" "}
+          <span className="pointTo">
+            <ArrowForwardRounded />
+          </span>{" "}
+        </h5>
+      </div>
+      <div className="projects">
+        <h5>
+          And check out some of my projects with this link here. <pre> </pre>{" "}
+          <span className="pointTo">
+            <ArrowForwardRounded />
+          </span>{" "}
+        </h5>
+      </div>
+      <div className="blog-info">
+        <h3>
+          Here you can see some of the Frontend Engineering Problems I am
+          testing myself on a daily basis to stay up to date and making sure I
+          keep a strong foundation! <br />{" "}
+          <div className="arrow-down">
+            <ArrowDownwardRounded />
+          </div>
+        </h3>
+      </div>
+      <div className="mail-me">
+        {" "}
+        <EmailRounded />
+      </div>
+      <div className="project-me">
+        {" "}
+        <ViewModuleRounded />
+      </div>
+      <div className="blog-projects">
+        <PlaylistAddCheckRounded />
+      </div>
+      <div className="sky">
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+      </div>
+      <div className="cloud"></div>
+      <div className="road"></div>
+    </div>
+  );
+};
+
+export default Landing;
